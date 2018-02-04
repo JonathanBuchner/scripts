97d2a7ccc0ad94781f8f9e5922fd5847011bf1da
Jonathan Buchner <websites@jonathanbuchner.com>
2018-02-03 21:16:09 -0800

    Add arrows and shadow

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@
 $(document).ready(
   () => {
     let cube = $('#cube');
+    let shadow = $('#shadow');
     let front = $('#cube .front')[0];
     let back = $('#cube .back')[0];
     let left = $('#cube .left')[0];
     let right = $('#cube .right')[0];
+    let title = $('h1');
 
     let moveCube = 'move-cube';
+    let moveShadow = 'move-shadow'
+    let moveTitle = 'move-title';
 
     let videos = [front, back, left, right];
     let videoReady = 0;
@@ -16,12 +20,14 @@ $(document).ready(
       (video) => {
         video.addEventListener('canplay',
          function() {
-            this.play();            
+            this.play();
           }
         );
       }
     );
 
     cube.addClass(moveCube);
+    shadow.addClass(moveShadow);
+    title.addClass(moveTitle);
   }
 );
